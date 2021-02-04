9621adc8e5af5586958a90ab84fa2721db93dd08
sro800313 <sro800313@gmail.com>
2021-02-04 14:14:45 +0800

    更新1

---
+++ Hello/src/main/java/com/itcast/maven/Hello.java
@@ -0,0 +1,8 @@
+package com.itcast.maven;
+
+public class Hello {
+	
+	public String sayHello(String name) {
+		return "Hello "+name+"!";
+	}
+}
---
+++ Hello/src/test/java/com/itcast/mavenTest/HelloTest.java
@@ -0,0 +1,16 @@
+package com.itcast.mavenTest;
+
+import org.junit.Test;
+
+import com.itcast.maven.Hello;
+
+public class HelloTest {
+	@Test
+	public void test() {
+		Hello hello = new Hello();
+		
+		String sayHello = hello.sayHello("maven");
+		System.out.println(sayHello);
+	
+	}
+}
